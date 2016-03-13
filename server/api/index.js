4bd715556081a498a8ee58b4d7eec998805fc111
Jon Borgonia <jon@gomagames.com>
2016-03-12 17:47:30 -1000

    simulate real api calls to m2x

--- server/api/index.js
+++ server/api/index.js
@@ -1,3 +1,4 @@
+'use strict';
 const express = require("express");
 const router = express.Router();
 const bodyParser = require('body-parser');
@@ -16,7 +17,7 @@ const M2X_KEY = process.env.M2X_APIKEY;
 // const device_id = "fbc1b926ecad75a3d53a4fad6257ea7d";
 const device_id = "3b0eaa66a0d88ade22a6af2b23378b72";
 
-var mock_data = JSON.stringify({
+var raw_data = {
   "jobs": [
     {
       "job_id": "job_name1",
@@ -106,7 +107,8 @@ var mock_data = JSON.stringify({
       "complete": false
     }
   ]
-})
+};
+var mock_data = JSON.stringify(raw_data);
 
 router.get('/test', (req,res)=>{
   res.send({ success : true });
@@ -115,7 +117,7 @@ router.get('/test', (req,res)=>{
 // meta-stream is a required non-numeric stream to have since it tracks other streams.
 router.get('/meta-stream', (req,res)=>{
   m2x.devices.streamValues( device_id, "meta-stream", function(response) {
-      console.log(JSON.parse(response.json.values[0].value));
+      // console.log(JSON.parse(respons .json.values[0].value));
       res.send(JSON.parse(response.json.values[0].value));
   });
 });
@@ -128,6 +130,42 @@ router.put('/meta-stream', (req,res)=>{
     res.send(response.json);
   });
 });
+
+// simulate
+setInterval(()=>{
+  let jobs = [];
+  for(let i=0,l=Math.ceil(Math.random()*4);i<l;i++){
+    let tasks = [];
+    for(let i=0,l=Math.ceil(Math.random()*4);i<l;i++){
+      tasks.push(
+        {
+          "node": "some_id"+i,
+          "name": "Task "+i+": Do something.",
+          "time_allocated": 360,
+          "completion_time": 0,
+          "complete": false,
+          "approved": false,
+          "contested": false,
+          "notes": "NOTES OR CONCERNS FOR MASTER TO READ ON CONTEST FLAG"
+        }
+      );
+    }
+    jobs.push({
+      "job_id": "job_name"+i,
+      "start_time": Date.now(),
+      "hard_time_limit": 6000,
+      "initator": "some boss dude",
+      "check_bypass": false,
+      "tasks": tasks,
+      "complete": false
+    });
+  }
+
+  let payload = {value : JSON.stringify({  jobs : jobs })};
+  m2x.devices.setStreamValue( device_id, "meta-stream", payload, function(response) {
+    console.log("posted",new Date(), response.json);
+  });
+}, 2130);
 
 // creates or updates a stream with the assigned ID
 // router.post('/task/:id', (req,res)=>{
